b184bbe929b719234e1611195168a17c0d101e65
Irfan Jalal <mindhunter@blinkenshell.org>
2023-06-25 10:07:39 +0530

    added journals.lastIncomingMessages()

--- inquirer.py
+++ inquirer.py
@@ -12,18 +12,21 @@ style = style_from_dict({
     })
 
 
-def display_list(no_chats,chats,id_list):
+def display_list(no_chats,chats,id_list,r_chats):
 
     if not no_chats:
         no_chats = 15
 
-    re_exps = [] 
-    dist = []
+    re_exps, dist, r_names = list(), list(), list() 
 
     if chats:
         for chat in chats:
             re_exp = r".*" + re.escape(chat) + r".*" 
             re_exps.append(re_exp) 
+    else: 
+        for chat in r_chats.data: 
+            r_names.append(chat['chatId'])
+    r_names = list(dict.fromkeys(r_names)) 
 
    #  for key in id_list.keys(): 
    #      for re_exp in re_exps: 
@@ -31,13 +34,19 @@ def display_list(no_chats,chats,id_list):
    #              print(key)
    #              exit(0) 
 
-    for key in id_list.keys():
-       for re_exp in re_exps: 
-           if re.search(re_exp,key,re.IGNORECASE):
-               tmp = {}
-               tmp['name'] = key
-               dist.append(tmp)
-    
+    for key,val in id_list.items():
+        if chats: 
+           for re_exp in re_exps: 
+               if re.search(re_exp,key,re.IGNORECASE):
+                   tmp = {}
+                   tmp['name'] = key
+                   dist.append(tmp)
+        else: 
+            for chat_id in r_names: 
+                if chat_id == val: 
+                   tmp = {}
+                   tmp['name'] = key
+                   dist.append(tmp)
     if dist: 
         questions = [
                {
